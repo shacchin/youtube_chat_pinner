1ef3cd8b83ffe2cdfc97d89c6b35a93d00e9b75c
Shacchin <sw20fd3s.ryota@gmail.com>
2019-01-15 23:58:44 +0900

    一旦正常に動くとこまで

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,32 +1,56 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
-var $ = require("jQuery");
+var $ = require('jQuery');
 
+console.log('comment pinner loaded')
 
+function pinComment() {
+  var commentFrame = $('iframe').contents();
 
-$(function() {
-  console.log("read youtube_extract Extention!!");
+  // モデレータコメント(スパナ)を取得
+  var modelator = commentFrame
+    .find('[aria-label=モデレーター]')
+    .closest('yt-live-chat-text-message-renderer');
 
-  function testOutput() {
+  modelator.attr('id', 'pinned-comment');
 
-    var iframe = $("iframe").contents();
-    var length = iframe.find("[id=author-name]").length;
-    console.log("全体コメント数："+length)
+  // モデレータコメントをコメント欄上部に表示
+  commentFrame.find('yt-live-chat-ticker-renderer').after(modelator);
+}
 
-    // モデレータカウント
-    var modelatorList = iframe.find("[aria-label=モデレーター]").length;
-    console.log("モデレータ数"+modelatorList);
+const setIntervalId = setInterval(setObserver, 1000);
 
-    // スパナマークコメント(モデレータ)を取得
-    var modelator = iframe.find("[aria-label=モデレーター]").closest("yt-live-chat-text-message-renderer");
+function setObserver() {
+  if ($('#chatframe').length) {
+    clearInterval(setIntervalId);
 
-    iframe.find("yt-live-chat-ticker-renderer").after(modelator)
+    $('#chatframe').on('load', () => {
+      // iframeのdocument取得
+      var iframe = document.getElementById('chatframe');
 
+      const doc = iframe.contentWindow.document; // iframe内文書の document
+
+      // コメント一覧取得
+      const comments = doc.getElementById('item-offset');
+
+      const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+          // console.log('change!!!'); // 変更された時の処理
+          pinComment();
+
+          // 削除用クリックイベントの追加
+          $(doc).on('click', '#pinned-comment', () => {
+            $(event.target).remove();
+          });
+        });
+      });
+
+      const config = {
+        childList: true,
+        attributes: true
+      };
+      observer.observe(comments, config); // 対象ノードとオブザーバの設定を渡す
+    });
   }
-
-  setInterval(testOutput,1000);
-
-  console.log("script end!!")
-
-});
+}
